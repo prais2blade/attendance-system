dcf2c7007ec44d8a2f83e9fe0bc062c5a674143f
Praise Ubga <praise2blade@gmail.com>
2026-06-20 21:16:29 +0100

    React frontend and backend restructure

--- backend/apps/reports/views.py
+++ backend/apps/reports/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils import timezone
 
-# Create your views here.
+from apps.students.models import Student
+from apps.attendance.models import Attendance
+
+
+def dashboard_stats(request):
+
+    today = timezone.localdate()
+
+    total_students = Student.objects.count()
+
+    present = Attendance.objects.filter(
+        date=today
+    ).count()
+
+    checked_out = Attendance.objects.filter(
+        date=today,
+        check_out__isnull=False
+    ).count()
+
+    absent = (
+        total_students - present
+    )
+
+    return JsonResponse({
+
+        "total_students":
+            total_students,
+
+        "present":
+            present,
+
+        "checked_out":
+            checked_out,
+
+        "absent":
+            absent
+
+    })
